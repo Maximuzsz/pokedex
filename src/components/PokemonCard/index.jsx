9443c913085b701e9aad3e122bdf68dbf4ff9134
Paulo Roberto <61876313+Maximuzsz@users.noreply.github.com>
2023-08-31 13:47:02 -0300

    adição dos pokemons e da pesquisa

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -4,20 +4,24 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import { TypeSpecimenOutlined } from '@mui/icons-material';
 
-export default function PokemonCard() {
+export default function PokemonCard({name, image, types}) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
+          height="200"
+          image={image}
           alt="green iguana"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {name}
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            {types[0].type.name}
           </Typography>
         </CardContent>
       </CardActionArea>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from  '../components/Navbar'
 import PokemonCard from  '../components/PokemonCard'
 import { Container, Grid } from '@mui/material'
 import axios from 'axios'
 
 export const Home = () => {
-  axios.get('https://pokeapi.co/api/v2/pokemon?limit=50')
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    getPokemons()
+  }, []);
+
+  const getPokemons = () => {
+    var endpoints =[]
+    for(var i=1; i<51; i++ ) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+    }
+    axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
+  }
+
+  const findPokemons = (name) => {
+    var filteredPokemons =[]
+    if(name==="") {
+      getPokemons()
+    }
+    for(var i in pokemons){
+      if(pokemons[i].data.name.includes(name)) {
+          filteredPokemons.push(pokemons[i])
+      }
+    }
+    setPokemons(filteredPokemons);
+  }
+  
   return (
     <div>
-      <Navbar/>
+      <Navbar findPokemon={findPokemons}/>
       <Container maxWidth='false' >
-        <Grid container>
-          <Grid item xs={3}>
-            <PokemonCard/>
-          </Grid>
-          <Grid item xs={3}>
-            <PokemonCard/>
-          </Grid>
-          <Grid item xs={3}>
-            <PokemonCard/>
-          </Grid>
-          <Grid item xs={3}>
-            <PokemonCard/>
-          </Grid>
+        <Grid container spacing={3}>
+          {pokemons.map((pokemon, key) =>(
+            <Grid item xs={2} key={key}>
+              <PokemonCard name={pokemon.data.name} image={pokemon.data.sprites.front_default} types={pokemon.data.types}/>
+            </Grid>
+          ))}
+          
         </Grid>
         
       </Container>
